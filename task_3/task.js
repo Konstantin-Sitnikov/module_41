67ffe0c7442dcac884e0a631c2112ce106f96d55
Konstantin <123>
2025-10-20 00:16:31 +0900

    next

--- task_3/task.js
+++ task_3/task.js
@@ -1,18 +1,86 @@
+const chatBody = document.querySelector(".chat__container--body")
 
 
-function clickButton() {
-  const widthWindow = window.screen.width
-  const heightWindow = window.screen.height
-  
-  const widthViewingArea = document.documentElement.clientWidth
-  const heightViewingArea = document.documentElement.clientHeight
+const connection = new WebSocket('wss://echo.websocket.org');
+//
 
-  const widthViewingAreaScroll = window.innerWidth
-  const heightViewingAreascroll = window.innerHeight
+connection.addEventListener("open", (event) => {
+
+	chatBody.innerHTML += 
+		`<span class="chat__text chat__text--servise-message">Соединение установлено.</span>`
+
+})
+
+
+connection.addEventListener("error", () => {
+
+	chatBody.innerHTML += 
+		`<span class="chat__text chat__text--servise-message">Что то пошло не так!!!</span>`
+
+})
 
 
 
-  window.alert(`Экран: ширина: ${widthWindow}, высота ${heightWindow}
-    Область просмотра: ширина: ${widthViewingArea}, высота ${heightViewingArea}
-    Область просмотра+ scroll: ширина: ${widthViewingAreaScroll}, высота ${heightViewingAreascroll}`)
+connection.addEventListener("message", (event) => {
+	
+	if (event.data.includes("Request")) {
+		return
+	}
+	
+
+	chatBody.innerHTML += 
+		`<div class="chat__container chat__container--message chat__container--server">
+		<span class="chat__text chat__text--message">${event.data}</span></div>`
+
+
+})
+
+
+function sendMesaage() {
+	const input = document.querySelector(".chat__input")
+
+	if (input.value === "") {
+		return
+	}
+
+	chatBody.innerHTML += 
+		`<div class="chat__container chat__container--message chat__container--user">
+		<span class="chat__text chat__text--message">${input.value}</span></div>`
+
+	connection.send(input.value)
+	input.value = ""
+	}
+
+
+const okPosition = (positon) => {
+	const latitude = positon.coords.latitude
+	const longitude = positon.coords.longitude
+	chatBody.innerHTML += 
+		`<div class="chat__container chat__container--message chat__container--user">
+            <a href="https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=8/${latitude}/${longitude}" class="chat__link">Вы тут</a>
+         </div>`
+
+}
+
+const errorPosition = () => {
+	chatBody.innerHTML += 
+		`<span class="chat__text chat__text--servise-message">Ошибка при получении позиции!</span>`
+
+}
+
+
+function geoLocation() {
+
+	if (!navigator.geolocation) {
+
+		chatBody.innerHTML += 
+		`<span class="chat__text chat__text--servise-message">Ваш браузер не потдерживает геолокацию</span>`
+
+	} else {
+		chatBody.innerHTML += 
+		`<span class="chat__text chat__text--servise-message">Определяем местоположение!</span>`
+		navigator.geolocation.getCurrentPosition(okPosition, errorPosition)
+	}
+	
+	
 }
